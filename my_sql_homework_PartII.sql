9c95610db939bf73050194286743bffc13f404ba
Terra Vaughn <vterra28@yahoo.com>
2021-03-04 17:54:44 -0600

    Homework assignment complete.

--- my_sql_homework_PartII.sql
+++ my_sql_homework_PartII.sql
@@ -11,31 +11,7 @@ from employees
 where hire_date between '01/01/86' and '12/31/86'; 
 
 -- Q3. List the manager of each department with the following information:
--- department number, department name, the manager's employee number, 
--- last name, first name.
--- Using subqueries [Subqueries didn't work. Keeping here just in case I need something like this later on.]
-
--- select dept_name
--- from departments
--- where dept_no in
--- 	(
--- 	select dept_no
--- 	from dept_manager
--- 	where emp_no in
--- 		(
--- 		select emp_no
--- 		from employees
--- 		where emp_title_id in
--- 			(
--- 			select title_id
--- 			from titles
--- 			where title = 'Manager'
--- 			)
--- 		 )
--- 	);
-
--- Trying with joins and create view.
--- create view department_managers as	
+-- department number, department name, the manager's employee number, last name, first name.
 
 select d.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name   
 from departments as d
@@ -63,20 +39,6 @@ where first_name = 'Hercules'
 and last_name like 'B%';
 
 -- Q6. List all employees in the Sales department, including their employee number, last name, first name and department name.
--- Missing department name
--- select emp_no, last_name, first_name
--- from employees
--- where emp_no in
--- 	(
--- 	select emp_no
--- 	from dept_emp
--- 	where dept_no in
--- 		(
--- 		select dept_no
--- 		from departments
--- 		where dept_name = 'Sales'
--- 		)
--- 	);
 
 select e.emp_no, e.last_name, e.first_name, p.dept_name   
 from departments as p
@@ -95,4 +57,12 @@ from departments as p
 		join employees as e
 		on (d.emp_no=e.emp_no)
 		where dept_name in ('Sales', 'Development');
+		
+-- Q8. In descending order, list the frequency count of employee last names, i.e, how many employees share each last name.
+
+ select last_name, count(last_name) as "Count by Last Name"
+ from employees
+ group by last_name
+ order by "Count by Last Name" desc;
+ 
 		
